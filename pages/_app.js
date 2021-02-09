7f8e7a6c0051cfc4d8a15fb1dd5efc9c6da28096
Benoit Rameau <rameaubenoit@hotmail.fr>
2021-02-09 15:12:44 +0100

    csp

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>ISS_TRACKING</title>
+        <meta httpEquiv="Content-Security-Policy" content="block-all-mixed-content" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
 import Head from "next/head"
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>ISS_TRACKING</title>
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-      </Head>
-    </>
-  )
+  return <></>
 }
