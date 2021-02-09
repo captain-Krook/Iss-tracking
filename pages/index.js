d3d78373db3f4671378f2d79c2bd78f24c6e766f
Benoit Rameau <rameaubenoit@hotmail.fr>
2021-02-09 15:27:48 +0100

    presentation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
-import Head from "next/head"
+import Link from "next/link"
 
 export default function Home() {
-  return <></>
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        margin: "auto",
+        height: "15em",
+        width: "50em",
+        marginTop: "10em",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        boxShadow: "0px 2px 15px -5px #000000",
+      }}
+    >
+      <Link href="/mapIss/map">
+        <h1 style={{ color: "white", cursor: "pointer" }}>ISS TRACKING</h1>
+      </Link>
+    </div>
+  )
 }
